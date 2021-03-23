4caff0d245ebf5855f119d69b7e6a1c3d369262b
Samiur-UTI <allpraisethelord@gmail.com>
2021-03-24 05:23:54 +0600

    schema dev init

--- server/src/configs/db.sql
+++ server/src/configs/db.sql
@@ -1,33 +1,43 @@
 CREATE TABLE userModel (
-    user_id VARCHAR(255) PRIMARY_KEY,
-    name VARCHAR(255) NOT_NULL,
-    username VARCHAR(255) NOT_NULL UNIQUE,
-    email VARCHAR(255) NOT_NULL,
-    owned_courses VARCHAR(255),
-    password PASSWORD VARCHAR(255),
-    comments VARCHAR(255),
-    FOREIGN KEY (owned_courses) REFERENCES (courses),
-    FOREIGN KEY (comments) REFERENCES (comments)
+    `user_id` VARCHAR(255) PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL UNIQUE,
+    `email` VARCHAR(255) NOT NULL,
+    `owned_courses` VARCHAR(255),
+    `password` VARCHAR(255),
+    `comments` VARCHAR(255),
+    `type` ENUM('User','Admin'),
+    FOREIGN KEY (`owned_courses`) REFERENCES `courses`(`course_id`),
+    FOREIGN KEY (`comments`) REFERENCES `comments`(`comment_id`)
+);
+CREATE TABLE instructorModel(
+    `user_id` VARCHAR(255) PRIMARY KEY,
+    `full_name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `courses` VARCHAR(255),
+    FOREIGN KEY ('courses') REFERENCES `courses`(`course_id`)
 );
 CREATE TABLE courses (
-   course_id VARCHAR(255) PRIMARY_KEY,
-   instructor_id VARCHAR(255)
-   name VARCHAR(255) NOT_NULL,
-   description VARCHAR(255) NOT_NULL,
-   playlist VARCHAR(255),
-   FOREIGN KEY (playlist) REFERENCES (videos)
+   `course_id` VARCHAR(255) PRIMARY KEY,
+   `instructor_id` VARCHAR(255),
+   `name` VARCHAR(255) NOT NULL,
+   `description` VARCHAR(255) NOT NULL,
+   `playlist` VARCHAR(255),
+   FOREIGN KEY (`instructor_id`) REFERENCES `instructorModel`(`user_id`),
+   FOREIGN KEY (`playlist`) REFERENCES `videos`(`video_id`)
 );
 CREATE TABLE videos (
-    video_id VARCHAR(255) PRIMARY_KEY,
-    name VARCHAR(255) NOT_NULL,
-    description VARCHAR(255) NOT_NULL,
-    API_KEY VARCHAR(255) NOT_NULL,
-    comments VARCHAR(255),
-    FOREIGN KEY (comments) REFERENCES (comments)
+    `video_id` VARCHAR(255) PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `API_KEY` VARCHAR(255) NOT NULL,
+    `comments` VARCHAR(255),
+    FOREIGN KEY `comments` REFERENCES `comments`(`comment_id`)
 );
 CREATE TABLE comments(
-    comment_id VARCHAR(255) PRIMARY_KEY,
-    content VARCHAR(255) NOT_NULL,
-    user_id VARCHAR(255) NOT_NULL,
-    FOREIGN KEY (user_id) REFERENCES (userModel)
+    `comment_id` VARCHAR(255) PRIMARY KEY,
+    `content` VARCHAR(255) NOT NULL,
+    `user_id` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `userModel`(`user_id`)
 );
